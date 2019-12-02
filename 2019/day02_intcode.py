a81b42436618c7498fbff9903b34815f0fb1b205
Andy Li <li.chenxing@gmail.com>
2019-12-02 13:46:02 +0800

    Add 2019 Day 2

---
+++ 2019/day02_intcode.py
@@ -0,0 +1,25 @@
+# https://adventofcode.com/2019/day/2
+
+from itertools import product
+from operator import add, mul
+
+OPERATION = {1: add, 2: mul}
+
+def fst_star(original, noun=12, verb=2): 
+    code = original.copy()
+    i, code[1], code[2] = 0, noun, verb
+    while code[i] != 99:
+        op, a, b, d = code[i:i+4]
+        code[d] = OPERATION[op](code[a], code[b])
+        i += 4
+    return code[0]
+
+def snd_star(code):
+    for noun, verb in product(range(100), repeat=2):
+        if fst_star(code, noun, verb) == 19690720:
+            return 100*noun + verb
+
+if __name__ == '__main__':
+    code = [*map(int, open('data/day02.in').read().split(','))]
+    print(fst_star(code))
+    print(snd_star(code))
---
+++ 2019/day03.py
@@ -0,0 +1,12 @@
+# https://adventofcode.com/2019/day/3
+
+def fst_star(data): 
+    pass
+
+def snd_star(data): 
+    pass
+
+if __name__ == '__main__':
+    data = [*map(int, open('data/day03.in'))]
+    print(fst_star(data))
+    # print(snd_star(data))
