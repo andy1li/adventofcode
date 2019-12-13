15ecedb965e3d43a01e0009e1c7d6fcb0e583f88
Andy Li <li.chenxing@gmail.com>
2019-12-13 14:38:44 +0800

    Add 2019 Day 13

--- 2019/day13.py
+++ 2019/day13.py
@@ -1,12 +1,42 @@
 # https://adventofcode.com/2019/day/13
 
-def fst_star(data): 
-    pass
+from intcode import run
+from matplotlib import pyplot as plt
+import numpy as np
 
-def snd_star(data): 
-    pass
+cmp = lambda a, b: (a > b) - (a < b)
+read = lambda g: (next(g), next(g), next(g))
+
+def get_screen(game):
+    screen = np.zeros([26, 40])
+    for _ in range(26*40):
+        x, y, t = read(game)
+        screen[y, x] = t
+    return screen
+
+def count_blocks(code): 
+    screen = get_screen(run(code, None))
+    # plt.imshow(screen); plt.show()
+    return (screen == 2).sum()
+
+def play_game(code): 
+    code[0], joystick = 2, [1]
+    game = run(code, iter(joystick))
+    screen = get_screen(game)
+
+    while True:
+        try: x, y, t = read(game) #; print(y, x, t)
+        except StopIteration: break
+        if x == -1: score = t; continue
+
+        screen[y, x] = t
+        if t == 3: paddle = x
+        if t == 4: # ball = x
+            joystick.append(cmp(x, paddle))
+            # plt.imshow(screen); plt.show()
+    print('score:', score)
 
 if __name__ == '__main__':
-    data = [*map(int, open('data/day13.in'))]
-    print(fst_star(data))
-    # print(snd_star(data))
+    code = [*map(int, open('data/day13.in').read().split(','))]
+    count_blocks(code)
+    play_game(code)
---
+++ 2019/day15.py
@@ -0,0 +1,12 @@
+# https://adventofcode.com/2019/day/1_
+
+def fst_star(data): 
+    pass
+
+def snd_star(data): 
+    pass
+
+if __name__ == '__main__':
+    data = [*map(int, open('data/day1_.in'))]
+    print(fst_star(data))
+    # print(snd_star(data))
