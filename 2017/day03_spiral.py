afb032c3ab6bb1b3d5f58e51dcb2e2233c8b4ff7
Andy Li <li.chenxing@gmail.com>
2021-09-18 15:25:39 +0800

    Add 2017 Day 03

--- 2017/day03_spiral.py
+++ 2017/day03_spiral.py
@@ -1,9 +1,45 @@
 # https://adventofcode.com/2017/day/3
 
-def get_pos(x):
-    return 0, 0
+from collections import defaultdict
+from itertools import count
+
+def spiral():
+    i = 1; yield 1, 0 # i, pos
+    for side_len in count(3, step=2):
+        pos = (1 - 1j) * (side_len//2)
+        dpos = 1j
+        for _ in range(4):
+            for _ in range(side_len-1):
+                i += 1; pos += dpos
+                yield i, pos
+            dpos *= 1j
+
+def fst_star(stop):
+    abs_int = lambda x: abs(int(x))
+    for x, pos in spiral():
+        if x == stop: 
+            return sum(map(abs_int, [pos.real, pos.imag]))
+
+def snd_star(stop):
+    grid = defaultdict(int, {0: 1})
+    for i, pos in spiral():
+        if i == 1: continue
+        grid[pos] = sum( 
+            grid[pos+dpos] 
+            for dpos in [
+                -1+1j,  1j, 1+1j, 
+                -1   ,      1, 
+                -1-1j, -1j, 1-1j, 
+            ]
+        )
+        if grid[pos] > stop: return grid[pos]
 
 if __name__ == '__main__':
-    assert checksum(parse(TEST1)) == 18
-    assert checksum(parse(TEST2), divisible) == 9
+    assert fst_star(1) == 0
+    assert fst_star(12) == 3
+    assert fst_star(23) == 2
+    assert fst_star(1024) == 31
+
+    print(fst_star(265149))
+    print(snd_star(265149))
 
