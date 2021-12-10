d597e490ec3ee0f3b33726b19228473af1f932a8
andy1li <1450947+andy1li@users.noreply.github.com>
2021-12-11 06:41:16 +0800

    Add 2021 Day 10

--- 2021/day10_.py
+++ 2021/day10_.py
@@ -1,32 +1,58 @@
 # https://adventofcode.com/2021/day/10
 
-def parse(raw):
-    command, x = raw.split()
-    return int(x) * DIR[command]
+from statistics import median
 
-def fst_star(commands): 
-    pos = sum(commands)
-    return int(pos.real * pos.imag) 
+EXPECTED = { ')': '(', ']': '[', '}': '{', '>': '<' }
+CORRUPT = { ')': 3, ']': 57, '}': 1197, '>': 25137 }
+COMPLETE = '([{<'
 
-def snd_star(commands):
-    pos = aim = 0
-    for c in commands:
-        if not c.real: aim += c
-        else         : pos += c * (1 + aim)
-    return int(pos.real * pos.imag) 
+def corrupt(line):
+    stack = []
+    for x in line:
+        if x in '([{<': stack += x,
+        elif x in ')]}>':
+            if stack[-1] != EXPECTED[x]: return CORRUPT[x]
+            stack.pop()
+    return 0
+
+def fst_star(lines): 
+    return sum(map(corrupt, lines))
+
+def complete(line):
+    stack = []
+    for x in line:
+        if x in '([{<': stack += x,
+        elif x in ')]}>':
+            stack.pop()
+    score = 0
+    for x in reversed(stack):
+        score *= 5
+        score += COMPLETE.index(x) + 1
+    return score
+
+def snd_star(lines):
+    return median(
+        map(complete,
+            filter(lambda x: not corrupt(x), lines)
+        )
+    )
 
 TEST = '''\
-forward 5
-down 5
-forward 8
-up 3
-down 8
-forward 2'''.splitlines()
+[({(<(())[]>[[{[]{<()<>>
+[(()[<>])]({[<{<<[]>>(
+{([(<{}[<>[]}>{[]{[(<()>
+(((({<>}<{<{<>}{[]{[]{}
+[[<[([]))<([[{}[[()]]]
+[{[{({}]{}}([{[{{{}}([]
+{<[[]]>}<{[{[{[]{()[[[]
+[<(<(<(<{}))><([]([]()
+<{([([[(<>()){}]>(<<{{
+<{([{{}}[<[[[<>{}]]]>[]]'''.splitlines()
 
 if __name__ == '__main__':
-    assert fst_star(map(parse, TEST)) == 150
-    assert snd_star(map(parse, TEST)) == 900
+    assert fst_star(TEST) == 26397
+    assert snd_star(TEST) == 288957
 
-    depths = list(map(parse, open('data/day10.in')))
-    print(fst_star(depths))
-    print(snd_star(depths))
+    lines = open('data/day10.in').readlines()
+    print(fst_star(lines))
+    print(snd_star(lines))
---
+++ 2021/day13_.py
@@ -0,0 +1,32 @@
+# https://adventofcode.com/2021/day/13
+
+def parse(raw):
+    command, x = raw.split()
+    return int(x) * DIR[command]
+
+def fst_star(commands): 
+    pos = sum(commands)
+    return int(pos.real * pos.imag) 
+
+def snd_star(commands):
+    pos = aim = 0
+    for c in commands:
+        if not c.real: aim += c
+        else         : pos += c * (1 + aim)
+    return int(pos.real * pos.imag) 
+
+TEST = '''\
+forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2'''.splitlines()
+
+if __name__ == '__main__':
+    assert fst_star(map(parse, TEST)) == 150
+    assert snd_star(map(parse, TEST)) == 900
+
+    depths = list(map(parse, open('data/day13.in')))
+    print(fst_star(depths))
+    print(snd_star(depths))
