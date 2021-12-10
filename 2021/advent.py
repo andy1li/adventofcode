08d20b15c7a03010d25c0bccbd6059b321641411
andy1li <1450947+andy1li@users.noreply.github.com>
2021-12-10 08:57:02 +0800

    Add 2021 Day 09

---
+++ 2021/advent.py
@@ -0,0 +1,18 @@
+def get_neighbors(y, x):
+    return [(y-1, x), (y, x+1), (y+1, x), (y, x-1)]
+
+def get_neighbor_items(grid, y, x):
+    for ny, nx in get_neighbors(y, x):
+        if within_bounds(grid, ny, nx):
+            yield (ny, nx), grid[ny][nx]
+
+def iterate(grid):
+    yield from (
+        (y, x, val)
+        for y, row in enumerate(grid)    
+        for x, val in enumerate(row)
+    )
+
+def within_bounds(grid, y, x):
+    height, width = len(grid), len(grid[0])
+    return 0 <= y < height and 0 <= x < width
---
+++ 2021/day09_bfs.py
@@ -0,0 +1,47 @@
+# https://adventofcode.com/2021/day/9
+
+from advent import get_neighbor_items, iterate
+from collections import Counter
+from functools import reduce
+from math import prod
+
+def fst_star(grid): 
+    return sum( 1 + int(val)
+        for (y, x, val) in iterate(grid)
+        if all( int(val) < int(nval)
+            for (ny, nx), nval in get_neighbor_items(grid, y, x)
+        )
+    )
+
+def snd_star(grid):
+    bfs = [ (y, x, val, (y, x))   
+        for (y, x, val) in iterate(grid)
+        if all( int(val) < int(nval)
+            for (ny, nx), nval in get_neighbor_items(grid, y, x)
+        )
+    ]
+    seen = {}
+    for (y, x, val, basin) in bfs:
+        if (y, x) in seen: continue
+        seen[y, x] = basin
+        for (ny, nx), nval in get_neighbor_items(grid, y, x):
+            if int(nval) > int(val) and int(nval) != 9:
+                bfs.append( (ny, nx, nval, basin) )
+    return prod( v
+        for k, v in Counter(seen.values()).most_common(3)
+    )
+
+TEST = '''\
+2199943210
+3987894921
+9856789892
+8767896789
+9899965678'''.splitlines()
+
+if __name__ == '__main__':
+    assert fst_star(TEST) == 15
+    assert snd_star(TEST) == 1134
+
+    grid = open('data/day09.in').read().splitlines()
+    print(fst_star(grid))
+    print(snd_star(grid))
