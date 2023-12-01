f79a34a3e3d449e7579486ea982cf1459fc4a562
Andy Li <1450947+andy1li@users.noreply.github.com>
2023-12-02 01:11:20 +0800

    WIP 2022 Day 15

--- 2022/day15_.py
+++ 2022/day15_.py
@@ -1,7 +1,16 @@
 # https://adventofcode.com/2022/day/15
 
-def parse(raw):
-    pass
+import re
+from collections import namedtuple
+
+Sensor = namedtuple('Sensor', 'x, y, r')
+
+def parse_sensors(raw):
+    def parse_sensor(raw):
+        PATTERN = r'Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)'
+        sx, sy, bx, by = list(map(int, re.match(PATTERN, raw).groups()))
+        return Sensor(sx, sy, abs(sx-bx)+abs(sy-by))
+    return list(map(parse_sensor, raw))
 
 def fst_star(data): 
     pass
@@ -9,13 +18,28 @@ def fst_star(data):
 def snd_star(data):
     pass
 
-TEST = ''''''.splitlines()
+TEST = '''\
+Sensor at x=2, y=18: closest beacon is at x=-2, y=15
+Sensor at x=9, y=16: closest beacon is at x=10, y=16
+Sensor at x=13, y=2: closest beacon is at x=15, y=3
+Sensor at x=12, y=14: closest beacon is at x=10, y=16
+Sensor at x=10, y=20: closest beacon is at x=10, y=16
+Sensor at x=14, y=17: closest beacon is at x=10, y=16
+Sensor at x=8, y=7: closest beacon is at x=2, y=10
+Sensor at x=2, y=0: closest beacon is at x=2, y=10
+Sensor at x=0, y=11: closest beacon is at x=2, y=10
+Sensor at x=20, y=14: closest beacon is at x=25, y=17
+Sensor at x=17, y=20: closest beacon is at x=21, y=22
+Sensor at x=16, y=7: closest beacon is at x=15, y=3
+Sensor at x=14, y=3: closest beacon is at x=15, y=3
+Sensor at x=20, y=1: closest beacon is at x=15, y=3'''.splitlines()
 
 if __name__ == '__main__':
-    data = parse(TEST)
+    sensors = parse_sensors(TEST)
+    print(sensors)
     # print(fst_star(data))
     # print(snd_star(data))
 
-    data = parse(open('data/day15.in'))
+    # data = parse_sensors(open('data/day15.in'))
     # print(fst_star(data))
     # print(snd_star(data))
