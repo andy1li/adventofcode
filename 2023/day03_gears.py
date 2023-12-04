c96122fad0332f867e2f75790f49310644374a82
Andy Li <1450947+andy1li@users.noreply.github.com>
2023-12-05 00:53:47 +0800

    Add 2023 Day 04

--- 2023/day03_gears.py
+++ 2023/day03_gears.py
@@ -2,6 +2,7 @@
 
 from collections import defaultdict
 from math import prod
+import re
 
 def scan(grid, row, start, end):
     nrows, ncols = len(grid), len(grid[row])
@@ -10,42 +11,35 @@ def scan(grid, row, start, end):
         for j in range(max(0, start-1), min(ncols, end+2))
     )
 
-def check_part_number(grid, row, start, end):
+def check_part(grid, row, start, end):
     return any(
         not grid[i][j].isdigit() and grid[i][j] != '.'
         for i, j in scan(grid, row, start, end)
     )
 
 def find_star(grid, row, start, end):
-    return next( ((i, j) 
+    return next(( (i, j) 
         for i, j in scan(grid, row, start, end) 
         if grid[i][j] == '*'
     ), None)
 
 def parse(grid):
-    for i, row in enumerate(grid):
-        start = None
-        for j, x in enumerate(row):
-            if x.isdigit() and start is None: 
-                start = j
-            if not x.isdigit() and j-1 >= 0 and row[j-1].isdigit():
-                if check_part_number(grid, i, start, j-1):
-                    yield int(row[start:j]), find_star(grid, i, start, j-1)
-                start = None
-
-        if start is not None and check_part_number(grid, i, start, j):
-            yield int(row[start:]), find_star(grid, i, start, j)
-
+    return [
+        (int(n.group()), find_star(grid, i, n.start(), n.end()-1))
+        for i, row in enumerate(grid)
+        for n in re.finditer(r'\d+', row)
+        if check_part(grid, i, n.start(), n.end()-1)
+    ]
+    
 def fst_star(parts):
     return sum(p[0] for p in parts)
 
 def find_gears(parts):
     stars = defaultdict(list)
     for number, star in parts:
-        if star is not None:
-            stars[star].append(number)
+        if star: stars[star].append(number)
     return ( numbers
-        for star, numbers in stars.items()
+        for numbers in stars.values()
         if len(numbers) == 2
     )
 
@@ -67,10 +61,10 @@ TEST = '''\
 .664.598..'''.splitlines()
 
 if __name__ == '__main__':
-    parts = list(parse(TEST))
+    parts = parse(TEST)
     assert fst_star(parts) == 4361
     assert snd_star(parts) == 467835
     
-    parts = list(parse(open('data/day03.in').read().splitlines()))
+    parts = parse(open('data/day03.in').read().splitlines())
     print(fst_star(parts))
     print(snd_star(parts))
